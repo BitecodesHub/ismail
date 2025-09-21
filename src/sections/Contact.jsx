c20cfd66acf2f6e39489d6b45ab8e59ec869208f
BiteCodesHub <bitecodes.global@gmail.com>
2025-09-21 13:31:08 +0530

    Contact

--- src/sections/Contact.jsx
+++ src/sections/Contact.jsx
@@ -94,7 +94,7 @@ const Contact = () => {
               name="name"
               type="text"
               className="field-input field-input-focus"
-              placeholder="John Doe"
+              placeholder="Your Name"
               autoComplete="name"
               value={formData.name}
               onChange={handleChange}
@@ -110,7 +110,7 @@ const Contact = () => {
               name="email"
               type="email"
               className="field-input field-input-focus"
-              placeholder="JohnDoe@email.com"
+              placeholder="youremail@gmail.com"
               autoComplete="email"
               value={formData.email}
               onChange={handleChange}
